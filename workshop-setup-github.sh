52355939a0867f3713fa5fd7557281f1fab72b48
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-05 11:38:34 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -2,4 +2,10 @@ myls=$(ls)
 echo "myls = $myls"
 ghFile=$1
 echo "ghFile = $1 = $ghFile"
-readarray -t ghUsers<$ghFile
+readarray -t ghUsers < $ghFile
+echo "after readarray"
+
+lines=`cat $ghFile`
+for line in $lines; do
+   echo "$line"
+done
