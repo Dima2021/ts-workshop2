e92457835ae9a0611787b95231b26a4a1edffe5c
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-05 09:22:43 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -1,8 +1,11 @@
+echo "ls"
+ls
+echo "end of ls"
 ghFile=$1
 readarray -t ghUsers < "$ghFile"
 ghUsersInd=0
-while (($ghUsersInd < ${#ghUsers[@]})); do
+while (($ghUsersInd < ${#ghUsers[@]}))
+do
     echo "Creating repository for ${ghUsers[$ghUsersInd]}"
-	
-	ghUsersInd=$(($ghUsersInd + 1))
+    ghUsersInd=$(($ghUsersInd + 1))
 done
