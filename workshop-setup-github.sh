221770d97c9bf1b55c8228993985d19843a0b23e
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-04 23:18:33 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -1,9 +1,3 @@
-# The following environments are controlled /set via repository secrets when executed via GitHub actions (workflows).
-#export GITHUB_ORGS=("mendts-workshop" "mendts-workshop1" "mendts-workshop2")
-#export GITHUB_USERNAME=ts-whitesource
-#export GITHUB_TOKEN=<get token from manager and set in shell before script>
-#export GH_USERS_PER_ORG=2
-
 ghFile=$1
-echo ghFile=$ghFile
-readarray -t ghUsers < $ghFile
+readarray -t ghUsers <$ghFile
+echo "ghFile=$1"
