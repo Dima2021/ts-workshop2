e7d4652e86ac2ba44f51d0b79b28a10174787477
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-04 23:48:16 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -1,6 +1,7 @@
 ghFile=$1
+readarray -t ghUsers <$ghFile
 
 # Delete the repocreated.txt file, in case it exists from the previous interactions.
 if [ -e "repocreated.txt" ]; then
-   rm repocreated.txt
+    rm repocreated.txt
 fi
