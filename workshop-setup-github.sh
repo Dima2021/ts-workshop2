e840db1c9dd2d2c095ea5fa5cd325e34dec94c6f
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-05 00:49:58 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -4,6 +4,7 @@ export GITHUB_USERNAME=ts-whitesource
 #export GITHUB_TOKEN=<get token from manager and set in shell before script>
 export GH_USERS_PER_ORG=2
 ghFile=$1
+ls
 echo "ghFile=$1"
 readarray -t ghUsers <$ghFile
 ghUsersInd=0
