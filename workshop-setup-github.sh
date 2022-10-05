a6ef4351d30c9a6fa8101d6ffedc98a5992c4289
Dima2021 <61808880+Dima2021@users.noreply.github.com>
2022-10-05 11:47:00 -0500

    Update workshop-setup-github.sh

--- workshop-setup-github.sh
+++ workshop-setup-github.sh
@@ -1,7 +1,8 @@
 myls=$(ls)
 echo "myls = $myls"
-ghFile=$1
-echo "ghFile = $1 = $ghFile"
+#ghFile=$1
+#echo "ghFile = $1 = $ghFile"
+ghFile="ghusers.txt"
 readarray -t ghUsers < $ghFile
 echo "after readarray"
 
