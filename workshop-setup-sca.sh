ab918e4886452bf6748667dbec3c4506c4ca5232
EC2 Default User <ec2-user@ip-172-31-16-222.ec2.internal>
2022-10-06 15:04:44 +0000

    updated workshop-setup-sca.sh

--- workshop-setup-sca.sh
+++ workshop-setup-sca.sh
@@ -1,15 +1,21 @@
-# The following environments are controlled /set via repository secrets when executed via GitHub actions (workflows).
 #export WS_APIKEYS=("apiKey" "apiKey1" "apiKey2")
 #export WS_USERKEYS=("userKey" "userKey1" "userKey2")
 #export WS_WSS_URL=https://saas.mend.io/api/v1.3
 #export GH_USERS_PER_ORG=2
-
 export WS_INVITER=facilitator@gmail.com
 export WS_USER_GROUP=admins
 
-
 emailsFile=$1
 readarray -t emails <$emailsFile
+
+# Remove all spaces and split WS_APIKEYS based on the delimiter ','
+WS_APIKEYS=$(echo $WS_APIKEYS | tr -d ' ')
+IFS=','; read -ra apiKeys <<< "$WS_APIKEYS"
+
+# Remove all spaces and split WS_APIKEYS based on the delimiter ','
+WS_USERKEYS=$(echo $WS_USERKEYS | tr -d ' ')
+IFS=','; read -ra userKeys <<< "$WS_USERKEYS"
+
 emailsInd=0
 while (($emailsInd < ${#emails[@]})); do
 #   echo "email $emailsInd is ${emails[$emailsInd]}"
@@ -30,18 +36,17 @@ while (($emailsInd < ${#emails[@]})); do
         assignedUsers=$(echo $assignedUsers | sed 's/,$//')
 
         # Uncomment for debug/troubleshooting purposes
-#       echo -e "\norgInd=$orgInd, APIKEY=${WS_APIKEYS[$orgInd]}, USERKEY=${WS_USERKEYS[$orgInd]}, emailsPerOrg=$emailsPerOrg, assignedUsers=$assignedUsers"
+#       echo -e "\norgInd=$orgInd, APIKEY=${apiKeys[$orgInd]}, USERKEY=${userKeys[$orgInd]}, emailsPerOrg=$emailsPerOrg, assignedUsers=$assignedUsers"
 
         echo -e "\nOrg #$orgInd: Invite Users to an Organization"
         curl --request POST -H "Content-Type:application/json" $WS_WSS_URL \
-       -d "{'requestType':'inviteUsers', 'userKey':'${WS_USERKEYS[$orgInd]}', 'orgToken':'${WS_APIKEYS[$orgInd]}', 'inviter':{'email':'$WS_INVITER'}, 'emails':[$emailsPerOrg]}"
+       -d "{'requestType':'inviteUsers', 'userKey':'${userKeys[$orgInd]}', 'orgToken':'${apiKeys[$orgInd]}', 'inviter':{'email':'$WS_INVITER'}, 'emails':[$emailsPerOrg]}"
 
         echo -e "\nOrg #$orgInd: Add Users to ${WS_USER_GROUP} Group"
         curl --request POST -H "Content-Type:application/json" $WS_WSS_URL \
-        -d "{'requestType':'addUsersToGroups', 'userKey':'${WS_USERKEYS[$orgInd]}', 'orgToken':'${WS_APIKEYS[$orgInd]}', 'assignedUsers':[[{'name':'$WS_USER_GROUP'}, [${assignedUsers}$
+        -d "{'requestType':'addUsersToGroups', 'userKey':'${userKeys[$orgInd]}', 'orgToken':'${apiKeys[$orgInd]}', 'assignedUsers':[[{'name':'$WS_USER_GROUP'}, [${assignedUsers}$
         # Init for the next Mend organization
         emailsPerOrg=""
         assignedUsers=""
     fi
 done
-		
