0b2fb3643861c98f6a5668bc2de4ec1cc079a9e4
EC2 Default User <ec2-user@ip-172-31-16-222.ec2.internal>
2022-10-06 23:46:30 +0000

    workshop-setup-sast.sh

--- workshop-setup-sast.sh
+++ workshop-setup-sast.sh
@@ -1,10 +1,14 @@
-# The following environments are controlled /set via repository secrets when executed via GitHub actions (workflows).
-#export SAST_API_TOKENS=("apiToken" "apiToken1" "apiToken2")
+#export SAST_API_TOKENS="apiToken, apiToken1, apiToken2"
 #export SAST_URL=https://sast.mend.io/sast/
 #export GH_USERS_PER_ORG=2
 
 emailsFile=$1
 readarray -t emails <$emailsFile
+
+# Remove all spaces and split SAST_API_TOKENS based on the delimiter ','
+SAST_API_TOKENS=$(echo $SAST_API_TOKENS | tr -d ' ' | tr  ',' ' ')
+read -ra apiTokens <<< "$SAST_API_TOKENS"
+
 emailsInd=0
 orgInd=0
 adminGroupID=""
@@ -14,7 +18,7 @@ while (($emailsInd < ${#emails[@]})); do
       # Calc organization index for API requests
       orgInd=$(($emailsInd / $GH_USERS_PER_ORG))
 
-      groups=$(curl -H "X-Auth-Token: ${SAST_API_TOKENS[$orgInd]}" $SAST_URL/api/groups)
+      groups=$(curl -H "X-Auth-Token: ${apiTokens[$orgInd]}" $SAST_URL/api/groups)
       # Contains the part of "groups" response before "Administrators"
       adminGroup=${groups/"Administrators"*/}
       # Contains the part of "adminGroup" after "id"
@@ -25,7 +29,7 @@ while (($emailsInd < ${#emails[@]})); do
    fi
 
    echo -e "\nOrg #$orgInd: Add user ${emails[$emailsInd]} to the Administrators group"
-   curl -H "X-Auth-Token: ${SAST_API_TOKENS[$orgInd]}" $SAST_URL/api/users \
+   curl -H "X-Auth-Token: ${apiTokens[$orgInd]}" $SAST_URL/api/users \
    -d '{"username":"'${emails[$emailsInd]}'","name":"'${emails[$emailsInd]}'","role":0,"groups":[{"id":"'$adminGroupID'","name":"Administrators"}]}'
 
    emailsInd=$(($emailsInd + 1))
